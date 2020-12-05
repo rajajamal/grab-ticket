03c45ed48d29558f349748705ebc23af37e4587a
rajajamal <aceh.rajajamal@gmail.com>
2020-12-06 03:42:21 +0700

    modified

--- next.config.js
+++ next.config.js
@@ -7,14 +7,14 @@ module.exports = (phase) => {
         env: {
             REDIRECT_URI: isDev
                 ? "http://localhost:3000/api/callback"
-                : "https://awesome-ticket-app.vercel.app/api/callback",
+                : "https://grab-ticket.vercel.app/api/callback",
             POST_LOGOUT_REDIRECT_URI : isDev
                 ? "http://localhost:3000/"
-                : "https://awesome-ticket-app.vercel.app/",
+                : "https://grab-ticket.vercel.app/",
             AUTH0_SCOPE : "openid email profile",
             SERVER_URL : isDev
                 ? "http://localhost:3000"
-                : "https://awesome-ticket-app.vercel.app",
+                : "https://grab-ticket.vercel.app",
         }
     }
 }
